a66f7025b8e65d604e4d9374989de7f2c9bfbda1
Spriha Jha <jha.spriha@gmail.com>
2023-09-09 14:16:03 -0400

    Updated with CORS headers

--- api/search.js
+++ api/search.js
@@ -1,10 +1,19 @@
-// api/search.js
-
 const Metaphor = require("metaphor-node");
 const metaphor = new Metaphor(process.env.METAPHOR_API_KEY);
 
 module.exports = async (req, res) => {
   try {
+    // Set CORS headers
+    res.setHeader("Access-Control-Allow-Origin", "*"); // Allow any origin
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST"); // Allow only GET and POST methods
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type"); // Allow only Content-Type header
+
+    // Handle preflight request. Don't run the rest of the route for a preflight request.
+    if (req.method === "OPTIONS") {
+      res.status(200).end();
+      return;
+    }
+
     const { queryString, numResults = 10 } = req.body;
     const response = await metaphor.search(queryString, {
       numResults,
